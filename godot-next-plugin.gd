49dba823384fc2cb36097e1aec5f41f093fe4ab9
Will Nations <willnationsdev@gmail.com>
2017-08-30 23:02:29 -0500

    removing 3.0 scripts from initial 2.1 branch commit

--- godot-next-plugin.gd
+++ godot-next-plugin.gd
@@ -2,7 +2,7 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("HoverContainer", "Container", preload("res://addons/godot-next/gui/containers/HoverContainer/HoverContainer.gd"), preload("res://addons/godot-next/gui/containers/HoverContainer/icon_hover_container.png"))
+    pass
 
 func _exit_tree():
-	remove_custom_type("HoverContainer")
+    pass
