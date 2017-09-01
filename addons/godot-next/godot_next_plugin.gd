ff6dafc1749d1985346772941849224a2ed6d427
Will Nations <willnationsdev@gmail.com>
2017-09-01 18:21:36 -0500

    Updated README w/ GDNative info and 2.1.4 issues

--- addons/godot-next/godot_next_plugin.gd
+++ addons/godot-next/godot_next_plugin.gd
@@ -2,8 +2,10 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("HoverContainer", "Container", preload("res://addons/godot-next/gui/containers/HoverContainer/HoverContainer.gd"), preload("res://addons/godot-next/gui/containers/HoverContainer/icon_hover_container.png"))
+	#add_custom_type("HoverContainer", "Container", preload("res://addons/godot-next/gui/containers/HoverContainer/HoverContainer.gd"), preload("res://addons/godot-next/gui/containers/HoverContainer/icon_hover_container.png"))
+	pass
 
 func _exit_tree():
-	remove_custom_type("HoverContainer")
+	#remove_custom_type("HoverContainer")
+	pass
 
